d6220ffa6cf8fab5e538d96d0f38a4126ef500fd
BOTXavier <xavier.dartevel@gmail.com>
2023-04-14 08:54:55 +0200

    template

---
+++ Caudrelier_Almero_Perrault_Dartevel_/html/Projet_cours/myApp/views.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config.from_object('myApp.config')
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/sgbd")
+def membres():
+    return render_template("sgbd.html")
+@app.route("/fichiers")
+def index():
+    return render_template("fichiers.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/logout")
+def logout():
+    return redirect('/login')
