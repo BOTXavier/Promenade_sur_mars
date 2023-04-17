7d2084226ab633106c2a9111a4648eb545e5aa76
BOTXavier <xavier.dartevel@gmail.com>
2023-04-17 20:16:49 +0200

    navigation fonctionne, images ajoutée

--- Caudrelier_Almero_Perrault_Dartevel_/myApp/views.py
+++ Caudrelier_Almero_Perrault_Dartevel_/myApp/views.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+
 app = Flask(__name__)
 app.config.from_object('myApp.config')
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/sgbd")
 def membres():
     return render_template("sgbd.html")
-@app.route("/fichiers")
-def fichiers():
-    return render_template("fichiers.html")
+
+@app.route("/elements.html")
+def elements():
+    return render_template("elements.html")
+
+@app.route("/left-sidebar")
+def leftsidebar():
+    return render_template("left-sidebar.html")
+
+@app.route("/right-sidebar")
+def rightsidebar():
+    return render_template("right-sidebar.html")
+
+@app.route("/no-sidebar")
+def nosidebar():
+    return render_template("no-sidebar.html")
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
 @app.route("/logout")
 def logout():
   return redirect('/login')
