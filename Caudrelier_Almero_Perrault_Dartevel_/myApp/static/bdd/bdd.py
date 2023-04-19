bf454d29cbbb3d742b14ed4aa3a70ee7ec279b1e
Louis-Yann <louis-yann.caudrelier@laposte.net>
2023-04-19 14:55:16 +0200

    bdd

--- Caudrelier_Almero_Perrault_Dartevel_/myApp/static/bdd/bdd.py
+++ Caudrelier_Almero_Perrault_Dartevel_/myApp/static/bdd/bdd.py
@@ -1,3 +1,15 @@
 import urllib.request
+
+DATA=[]
+
+#for i in range(2001):
+#    with urllib.request.urlopen('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='+str(i)+'&api_key=DEMO_KEY') as f:
+#        DATA.append(eval(f.read().decode('utf-8')))
+
+
 with urllib.request.urlopen('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY') as f:
-    print(f.read())
+    data=f.read().decode('utf-8')
+    print(type(data))
+    datadico=eval(data)
+    print(type(datadico))
+
