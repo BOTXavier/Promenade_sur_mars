99a64212d7f7653e89b0e5435cc6377b4ffbc28f
Louis-Yann <louis-yann.caudrelier@laposte.net>
2023-05-09 14:44:28 +0200

    BDD

--- Caudrelier_Almero_Perrault_Dartevel_/myApp/model/NASAbdd.py
+++ Caudrelier_Almero_Perrault_Dartevel_/myApp/model/NASAbdd.py
@@ -1,4 +1,5 @@
 import urllib.request
+import json
 import time
 
 ###################################################################################
@@ -8,12 +9,14 @@ import time
 
 ###################################################################################
 
-ROVER=['spirit','opportunity','curiosity','perseverance']
-API_KEY=['09r9lOKEqz9yGsPK6iiKDFFKRBLEvl7ZQKXyNaoS']
+ROVER=['perseverance','spirit','opportunity','curiosity']
+API_KEY=['uFPg2xAvVYtXreHwgx9swNodGOnuqRCsqRX426T6','09r9lOKEqz9yGsPK6iiKDFFKRBLEvl7ZQKXyNaoS']
+LIENS_POSI=['https://mars.nasa.gov/mmgis-maps/M20/Layers/json/M20_waypoints.json'] # A ordonner dans le même ordre que ROVER
 
 dico_photos={}
 dico_rover={}
 dico_camera={}
+dico_posi={}
 
 def réordonnencement(fichier):
     data=fichier.read().decode('utf-8')
@@ -31,33 +34,73 @@ def réordonnencement(fichier):
             camera.pop('id')
             dico_camera[camera_id]=camera
             
-def check_NASA(compt,rover,sol,compt_api_key):
+def check_NASA(compt_req,rover,sol,compt_api_key):
     url='https://api.nasa.gov/mars-photos/api/v1/rovers/'+rover+'/photos?sol='+str(sol)+'&api_key='+API_KEY[compt_api_key]
     with urllib.request.urlopen(url) as fichier:
          réordonnencement(fichier)
-         compt+=1
-         print(compt,len(dico_photos))
-         return compt
-
+         compt_req+=1 # une requête  a été effectuée
+    return compt_req
 
 def data_base():
     data={"photos":[0]}
     compt_api_key=0
-    compt=0 # compteur pour limiter le nombre de requêtes à 1000 par heures
+    compt_req=0 # compteur pour limiter le nombre de requêtes à 1000 par heures
     for rover in ROVER:
         sol=-1
         while len(data["photos"])!=0:
             sol+=1
-            # print(compt,sol)
-            if compt==100 and compt_api_key<len(API_KEY)-1:
+            if compt_req==5 and compt_api_key<len(API_KEY)-1: # Si on a atteint le nombre de requête maximale pour la clé, on passe à la suivante
                 compt_api_key+=1
-                compt=0
-            elif compt==100:
+                compt_req=0
+            elif compt_req==5: # Si on a atteint le nombre maximal de requête pour toutes les clés, on attend 1h
                 time.sleep(10)
-                compt=check_NASA(compt, rover, sol, compt_api_key)
+                compt_req=check_NASA(compt_req, rover, sol, compt_api_key)
             else:
-                compt=check_NASA(compt, rover, sol, compt_api_key)
-            print (dico_camera) #,dico_rover,dico_camera
-    
+                compt_req=check_NASA(compt_req, rover, sol, compt_api_key)
+                
 
-data_base()
+
+
+def sol_max(rover_id):
+    sol_max_rov=0
+    for photos_id in dico_photos:
+        photo=dico_photos[photos_id]
+        sol_photo=photo['sol']
+        if photo['rover']==rover_id and sol_photo>sol_max_rov:
+            sol_max_rov=sol_photo
+    return sol_max_rov
+
+def deter_rover_id_nom(rover):
+    for rover_id_l in dico_rover:
+        if dico_rover[rover_id_l]['name']==rover:
+            return rover_id_l
+
+def deter_rover_id_id(rover_id):
+    for rover_id_l in dico_rover:
+        if dico_rover[rover_id_l]['name']==ROVER[rover_id].title():
+            return rover_id_l
+
+    
+def réordonnencement_posi(fichier,rover_id):
+    data=json.load(fichier)
+    dico_posi[rover_id]={}
+    dico_posi_rov=dico_posi[rover_id]
+    sol_max_rov=sol_max(rover_id)
+    data_posi_0=data['features'][0]['properties']
+    dico_posi_rov[0]={'lat':data_posi_0['lat'],'long':data_posi_0['lon'],'cap':data_posi_0['yaw']}
+    for d in range(1,sol_max_rov):
+        dico_posi_rov[d]=dico_posi_rov[d-1]
+        data_posi_d=data['features'][d]['properties']
+        if data_posi_d['lat']!=dico_posi_rov[d]['lat'] or data_posi_d['lon']!=dico_posi_rov[d]['long'] or data_posi_d['yaw']!=dico_posi_rov[d]['cap']:
+            dico_posi_rov[d]={'lat':data_posi_d['lat'],'long':data_posi_d['lon'],'cap':data_posi_d['yaw']}
+    
+def check_posi():
+    rover_id=0
+    for lien in LIENS_POSI:
+        with urllib.request.urlopen(lien) as fichier_posi:
+            réordonnencement_posi(fichier_posi,deter_rover_id_id(rover_id))
+        rover_id+=1
+
+
+#data_base()
+#check_posi()
