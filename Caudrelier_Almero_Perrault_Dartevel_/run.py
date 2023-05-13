115cdd77daadfe079a48c5b59daf44d8576b42a3
almero <hugoalmero@gmail.com>
2023-05-13 18:20:34 +0200

    a

--- Caudrelier_Almero_Perrault_Dartevel_/run.py
+++ Caudrelier_Almero_Perrault_Dartevel_/run.py
@@ -1,8 +1,12 @@
 from myApp.config import DEBUG, WEB_SERVER
 from myApp.views import app
+import myApp.model.remplissage_BDD
 if __name__ == '__main__':
     app.run(
         host = WEB_SERVER['host'],
         port= WEB_SERVER['port'],
         debug=DEBUG
     )
+
+
+
