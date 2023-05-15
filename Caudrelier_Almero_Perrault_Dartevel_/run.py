71c49bc21fbdb14068a0182e7f9deceb900363f7
almero <hugoalmero@gmail.com>
2023-05-15 14:12:43 +0200

    CORRECTION BUG

--- Caudrelier_Almero_Perrault_Dartevel_/run.py
+++ Caudrelier_Almero_Perrault_Dartevel_/run.py
@@ -1,6 +1,6 @@
 from myApp.config import DEBUG, WEB_SERVER
 from myApp.views import app
-import myApp.model.remplissage_BDD
+#import myApp.model.remplissage_BDD
 if __name__ == '__main__':
     app.run(
         host = WEB_SERVER['host'],
